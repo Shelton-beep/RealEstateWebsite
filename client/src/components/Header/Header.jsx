3cc39af7eaeea1701a928f0c7aa0cc758ae93f88
Shelton Simbi <shltnsimbi@gmail.com>
2023-07-14 09:01:06 +0200

    Added Header component section

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+//import image from "../../assets/images/house2.jpg";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container flexCenter paddings innerWidth">
+        <div>
+          <p>Shutto Estate</p>
+          {/*<img src={image} alt="header image" width={100} />*/}
+        </div>
+
+        <div className="flexCenter h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
