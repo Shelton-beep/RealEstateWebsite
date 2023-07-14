bf159b44da0982e466b1faf2f57361e6c676663e
Shelton Simbi <shltnsimbi@gmail.com>
2023-07-14 16:17:25 +0200

    Added Residencies section and its contents

---
+++ client/src/utils/slider.js
@@ -0,0 +1,53 @@
+import image1 from "../assets/images/house1.jpg";
+import image2 from "../assets/images/house2.jpg";
+import image3 from "../assets/images/house3.jpg";
+import image4 from "../assets/images/house4.jpg";
+import image5 from "../assets/images/house5.jpg";
+import image6 from "../assets/images/house6.jpg";
+
+const data = [
+  {
+    name: "Aliva Priva Jardin",
+    price: "47,043",
+    detail:
+      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    image: image1,
+  },
+  {
+    name: "Asatti Garden City",
+    price: "66,353",
+    detail:
+      "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    image: image2,
+  },
+  {
+    name: "Citralan Puri Serang",
+    price: "35,853",
+    detail:
+      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    image: image3,
+  },
+  {
+    name: "Aliva Priva Jardin",
+    price: "47,043",
+    detail:
+      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    image: image4,
+  },
+  {
+    name: "Asatti Garden City",
+    price: "66,353",
+    detail:
+      "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    image: image5,
+  },
+  {
+    name: "Citralan Puri Serang",
+    price: "35,853",
+    detail:
+      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    image: image6,
+  },
+];
+
+export default data;
