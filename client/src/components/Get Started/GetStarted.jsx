09abbe1f2223cac65062a881d3fd3e3ccd33b049
Shelton Simbi <shltnsimbi@gmail.com>
2023-07-16 09:40:22 +0200

    Added Get Started section

---
+++ client/src/components/Get Started/GetStarted.jsx
@@ -0,0 +1,23 @@
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Shutto Estate</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+          <button className="button">
+            <a href="mailto:shltnsimbi@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
